eeb87b61967afbcdc6e599e6e4f581f9c85f7812
RRoggia <renanzr@gmail.com>
2018-12-02 23:13:32 -0200

    solve second part of second day

--- src/com/rroggia/day2/Day2.java
+++ src/com/rroggia/day2/Day2.java
@@ -1,8 +1,6 @@
 package com.rroggia.day2;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.rroggia.utils.IOUtils;
 
@@ -12,30 +10,40 @@ public class Day2 {
 
 		List<String> boxIDs = IOUtils.readStringsFromInputFile("./src/com/rroggia/day2/input");
 
-		int boxIDsWithTwoLetters = 0;
-		int boxIDsWithThreeLetters = 0;
+		String commonLetters = "";
 
 		for (String boxID : boxIDs) {
+			for (String nextBoxId : boxIDs) {
 
-			Map<String, Integer> letters = new HashMap<String, Integer>();
+				boolean hasOneDifferentiation = false;
 
-			for (int i = 0; i < boxID.length(); i++) {
-				String key = Character.toString(boxID.charAt(i));
+				for (int i = 0; i < boxID.length(); i++) {
+					String key = Character.toString(boxID.charAt(i));
+					String nextBoxKey = Character.toString(nextBoxId.charAt(i));
 
-				if (letters.containsKey(key))
-					letters.put(key, letters.get(key) + 1);
-				else
-					letters.put(key, 1);
+					if (key.equals(nextBoxKey)) {
+						commonLetters += key;
+						continue;
+					}
+
+					if (hasOneDifferentiation) {
+						hasOneDifferentiation = false;
+						break;
+					} else {
+						hasOneDifferentiation = true;
+					}
+				}
+
+				if (hasOneDifferentiation) {
+					break;
+				} else {
+					commonLetters = "";
+				}
 			}
-
-			if (letters.values().contains(Integer.valueOf(2)))
-				boxIDsWithTwoLetters++;
-
-			if (letters.values().contains(Integer.valueOf(3)))
-				boxIDsWithThreeLetters++;
-
+			if (!commonLetters.isEmpty())
+				break;
 		}
 
-		System.out.println(boxIDsWithTwoLetters * boxIDsWithThreeLetters);
+		System.out.println(commonLetters);
 	}
 }
